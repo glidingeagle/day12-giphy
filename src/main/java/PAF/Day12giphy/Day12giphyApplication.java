bf8ebade603f81489896978a799fd1a6242a076f
glidingeagle <100522461+glidingeagle@users.noreply.github.com>
2022-04-05 11:07:25 +0800

    initial commit

---
+++ src/main/java/PAF/Day12giphy/Day12giphyApplication.java
@@ -0,0 +1,13 @@
+package PAF.Day12giphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12giphyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12giphyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PAF/Day12giphy/Day12giphyApplicationTests.java
@@ -0,0 +1,13 @@
+package PAF.Day12giphy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day12giphyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
